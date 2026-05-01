f680caa443bf922b85a1fdeec2dbda5c01a0f9f1
Sascha Cherevko <mr.sascha.cherevko@gmail.com>
2026-05-01 09:02:59 +0200

    removed theme spec from lualine

--- lua/curry/plugins/visuals/lualine.lua
+++ lua/curry/plugins/visuals/lualine.lua
@@ -2,6 +2,6 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-        require("lualine").setup({ options = { theme = "ayu_dark" }})
+        require("lualine").setup()
     end
 }
